ca9634c134a3daeca90eb2f093fe6336ea820e48
Adam Klein <adam3207@gmail.com>
2024-11-03 05:10:08 -0800

    improved table

--- src/dot_test.py
+++ src/dot_test.py
@@ -8,21 +8,41 @@ import matplotlib.pyplot as plt
 def main():
     
     zs = []
+    for n in tqdm(range(2, 2048, 100)):
 
-    for n in tqdm(range(2, 1025)):
+        x = np.random.randn(1000, n)
+        x /= np.linalg.norm(x, axis=-1, keepdims=True)
+        
+        z = np.abs((x @ x.T))
+        z /= z.sum(axis=0)
 
-        x = np.random.randn(n, 50)
-        y = np.random.randn(50, n)
-
-        x /= np.linalg.norm(x, axis=0, keepdims=True)
-        y /= np.linalg.norm(y, axis=-1, keepdims=True)
-
-        z = np.abs((y @ x))
-        z = z.mean()
+        z = np.diagonal(z).mean()
 
         zs.append(z)
 
-    plt.plot(range(2, 1025), zs)
+    plt.plot(range(2, 2048, 100), zs, label="linear")
+
+    ds = []
+    zs = []
+    for n in tqdm(range(2, 64)):
+        ds.append(n + n**2)
+
+        x = np.random.randn(1000, n)
+        x /= np.linalg.norm(x, axis=-1, keepdims=True)
+
+        y = (x[:, :, None] @ x[:, None, :])
+        x = np.concatenate([x * 2, y.reshape(1000, -1)], axis=-1)
+
+        z = np.abs(x @ x.T)
+        z /= z.sum(axis=0)
+
+        z = np.diagonal(z).mean()
+
+        zs.append(z)
+
+    plt.plot(ds, zs, label="taylor")
+
+    plt.legend()
     plt.show()
 
 
