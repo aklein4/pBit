a33944d6449cdb568442fbc3c8a77fafa4a64faf
Adam Klein <adam3207@gmail.com>
2024-11-13 19:19:07 -0800

    zero attention squared

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -248,7 +248,8 @@ class ZeroAttention(nn.Module):
         attn_weights = torch.matmul(query_states, key_states.transpose(2, 3) / self.head_dim)
 
         # apply non-linearity
-        attn_weights = F.logsigmoid(-attn_weights).pow(2) - self.alpha * (np.log(2)**2) + attn_weights/100
+        # attn_weights = F.logsigmoid(-attn_weights).pow(2) - self.alpha * (np.log(2)**2) + attn_weights/100
+        attn_weights = attn_weights.pow(2)
 
         # attn_weights = attn_weights.exp() - 1
         # attn_weights = attn_weights / (1e-5 + torch.sqrt(attn_weights.pow(2).sum(dim=-1, keepdim=True)))
@@ -256,15 +257,17 @@ class ZeroAttention(nn.Module):
         if attention_mask is not None:
             attn_weights = attn_weights * torch.exp(attention_mask) # zero where -inf
 
+        attn_weights = attn_weights / (1 + attn_weights.sum(dim=-1, keepdim=True))
+
         # get output
         attn_output = torch.matmul(attn_weights, value_states)
         attn_output = attn_output.transpose(1, 2)
 
         # apply layer norm
-        attn_output = F.normalize(attn_output + self.b, p=2, dim=-1) * np.sqrt(self.head_dim)
+        # attn_output = F.normalize(attn_output + self.b, p=2, dim=-1) * np.sqrt(self.head_dim)
         attn_output = attn_output.reshape(bsz, q_len, self.qkv_size)
 
-        return self.O((attn_output * self.affine) + self.out_b)
+        return self.O(attn_output) # self.O((attn_output * self.affine) + self.out_b)
 
 
 class RotaryEmbedding(nn.Module):
